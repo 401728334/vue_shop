c8e0109e167cb28bd19b945dd58a2d0561a277cd
鸽语 <401728334@qq.com>
2021-06-13 15:02:13 +0800

    add files

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login_box">
+
+    </div>
+</template>
+
+<script lang="ts">
+export default ({
+
+})
+</script>
+
+<style lang="less" scoped>
+    .login_box{
+        height:100px;
+        background-color: aqua;
+    }
+</style>
